6e907f465fa2956e7ff3a8908facf0fe6acd07db
Carlos Gubio <carlosgubio@gmail.com>
2022-11-19 21:20:50 -0300

    .

--- StudioPilates/Pages/Index.cshtml.cs
+++ StudioPilates/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using StudioPilates.Data;
 using StudioPilates.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace StudioPilates.Pages
@@ -16,15 +20,33 @@ namespace StudioPilates.Pages
 
         public IList<Customer> Customers;
 
-        public IndexModel(ILogger<IndexModel> logger, StudioPilatesContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+        //public IndexModel(ILogger<IndexModel> logger, StudioPilatesContext context)
+        //{
+        //    _logger = logger;
+        //    _context = context;
+        //}
 
         //public async Task OnGetAsync()
         //{
         //    Customers = await _context.Customers.ToListAsync<Customer>();
         //}
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public string CaminhoImagem { get; set; }
+
+        //[BindProperty]
+        //[Display(Name = "Imagem da Logo")]
+        //[Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
+        //public IFormFile ImagemLogo { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, StudioPilatesContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+            CaminhoImagem = "~/img/LOGO-IMC-0002.jpg";
+        }
+
     }
+
 }
